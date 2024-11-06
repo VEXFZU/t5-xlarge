00d9390c37ae102640706233d2a3b1ca93e300d9
SabaPivot <careforme.dropout@gmail.com>
2024-11-06 06:02:16 +0000

    Fix: Changed compute_metrics to match decoding format
    Solved issue 1: When predict_with_generate = True, default skip_sepcial_tokens = True. In this case, no braille is printed.
    Solved issue 2: Must extract token_ids from predicted logits, and match the batch_decode size.
    
    All issues solved.

--- inference.py
+++ inference.py
@@ -5,16 +5,16 @@ from transformers import (
 )
 from data import read_braille_tokens, add_braille_tokens
 
-model = AutoModelForSeq2SeqLM.from_pretrained("/home/careforme.dropout/braille/results/241104/checkpoint-2800")
+model = AutoModelForSeq2SeqLM.from_pretrained("/home/careforme.dropout/t5-large/results/241105/checkpoint-1224")
 tokenizer = AutoTokenizer.from_pretrained("KETI-AIR/ke-t5-large-ko", legacy=False)
 braille_dict = read_braille_tokens()
-tokenizer = add_braille_tokens(braille_dict, tokenizer, model)
+add_braille_tokens(braille_dict, tokenizer, model)
 
 # Move model to GPU if available
 if torch.cuda.is_available():
     model.to("cuda")
 
-def translate_text(text, source_lang="한국어", target_lang="점자", max_length=256):
+def translate_text(text, source_lang="한국어", target_lang="점자", max_length=128):
     input_text = f"{source_lang}를 {target_lang}로 변환하세요.\n{source_lang}: {text}\n{target_lang}:"
     inputs = tokenizer(input_text, return_tensors="pt", max_length=max_length, truncation=True)
 
@@ -29,12 +29,11 @@ def translate_text(text, source_lang="한국어", target_lang="점자", max_leng
         num_beams=4,
         early_stopping=True
     )
-    print(outputs)
 
     # Decode the output tokens
-    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=False)
-    return translated_text
+    # translated_text = tokenizer.decode(outputs[0], skip_special_tokens=False)
+    return outputs
 
 # Test translation
-ko = "안전 감지 기능 면에서도 일반 산업용 로봇과 차별화된다. ZKW에 따르면 ‘코봇’은 이동 중에 저항이나 충돌을 감지하면 센서가 해당 정보를 전장설계(PLC) 제어에 전달한다. 그 이후 로봇은 즉시 작업을 중단한다."
-print(translate_text(ko))
+ko = "우주소녀가 유니세프에 기부를 했다."
+print(tokenizer.batch_decode(translate_text(ko)))
