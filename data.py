deda1862682362630b78ad7e0cf03f7099f31de0
azaraks <35026336+azaraks@users.noreply.github.com>
2024-11-09 03:02:24 +0900

    Chore: VEX-59 t5-large train script에서 실행에 관련된 인자들을 분리한다.

--- data.py
+++ data.py
@@ -19,10 +19,15 @@ def load_data(data_dir):
     return dataset
 
 def read_braille_tokens():
+    # Returns a list of Braille Unicode characters.
+    # All 6-dot Braille patterns are in the range 0x2800 to 0x2840.
+    # But we need to use 'space' character instead of Braille Pattern Blank (0x2800)
+    # Because the tokenizer will split the text by space.
+    # Using Hexadecimal numbers for better understanding of Unicode characters.
     return [chr(i) for i in range(0x2801, 0x2840)]
 
-def add_braille_tokens(braille_token, tokenizer, model):
-    special_tokens_dict = {"additional_special_tokens": braille_token}
+def add_braille_tokens(tokenizer, model):
+    special_tokens_dict = {"additional_special_tokens": read_braille_tokens()}
     tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
     return tokenizer
